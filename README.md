82cca34e34a768f99e9de11c216f1bb015ce4d0f
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-03-01 09:47:02 +0100

    add demo

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # n8n-nodes-changed
-n8n node to detect if something changed between two executions
+
+n8n node to detect if something changed between the current execution and the previous one.
+
+[![n8n](https://github.com/naskio/n8n-nodes-changed/blob/main/assets/screenshot.png?raw=true)](https://nask.io)
+
+# Description
+
+This node take an input and redirect the stream based on if the input has changed between the current and the previous
+execution or not.
+
+# Contribute
+
+Pull requests are welcome! For any bug reports, please create
+an [issue](https://github.com/naskio/n8n-nodes-changed/issues).
+
+See the [contributing guide](./CONTRIBUTING.md) for more information.
+
+# License
+
+[GPL-3.0](./LICENSE)
--- demo/run_demo.sh
+++ demo/run_demo.sh
@@ -5,16 +5,19 @@ DEMO_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 echo DEMO_DIR: $DEMO_DIR
 
-quick=${QUICK:-true} # set to false to install all dependencies
+quick=${1:-true} # set to false to install all dependencies
 
-if [[ ! -d ${DEMO_DIR}/.node_modules/ ]]; then
+if [[ ! -d "${DEMO_DIR}/node_modules" ]]; then
 	echo "Installing node modules..."
+	cd ${DEMO_DIR}/
 	yarn install
+	cd ./../
 fi
 
 if [ $quick = true ]; then
 	cd ${DEMO_DIR}/
 	cd ./../
+	echo "Building n8n-node ..."
 	yarn run build
 	cd ${DEMO_DIR}/
 	yarn run start
@@ -33,9 +36,9 @@ else
 	#### --------------------------------------------------
 	# run n8n locally
 	cd ${DEMO_DIR}/
-	yarn install
-	yarn unlink "n8n-nodes-changed"
 	yarn link "n8n-nodes-changed"
+	#	yarn install
+	#	yarn unlink "n8n-nodes-changed"
 	# yarn add n8n-nodes-changed
 	yarn run start
 fi
